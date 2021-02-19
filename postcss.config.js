dc4cbbd0db383b18ce192d5599aa0be0e646eef7
Matheus Silva de Freitas <matheus.freitas@autoavaliar.local>
2021-02-18 23:03:24 -0300

    Removing purge css

--- postcss.config.js
+++ postcss.config.js
@@ -15,6 +15,6 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    // ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 };
